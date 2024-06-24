fcb4027c6186ad931c0be7f2f4a96cea48f94a77
manuguzmanx <luisvg18@gmail.com>
2024-06-24 00:07:44 -0600

    Cambios

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,16 +3,17 @@ import { RouterOutlet } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
 import { CalendarModule } from 'primeng/calendar';
 import { TableModule } from 'primeng/table';
+import { CheckboxModule } from 'primeng/checkbox';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ButtonModule, TableModule, CalendarModule],
+  imports: [RouterOutlet, ButtonModule, TableModule, CalendarModule, CheckboxModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'ngsitioguzman';
+  title = 'Habits Tracker';
 
   date: Date[] | undefined;
 
